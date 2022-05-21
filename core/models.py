bf7179943fd88ff3b99b80b41cc28ef9211239b6
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-21 11:50:46 -0300

    novo campo local e tornado os campos obrigatórios

--- core/models.py
+++ core/models.py
@@ -8,6 +8,7 @@ class Evento(models.Model):
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local = models.CharField(max_length=25)
 
     class Meta:
         db_table = 'evento'
